3f1fb189051283e7535f88ff2ea13dcbca651b3f
avetisyan333 <harutavetisyan0@gmail.com>
2022-10-22 17:42:52 +0400

    Updates

---
+++ precache-manifest.6e37d09831bb525df41e037e25a17e27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bf334c739c248b818e258f887be91d",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "30c1242facc853fc0988",
+    "url": "/quiz/static/css/main.8d2c575b.chunk.css"
+  },
+  {
+    "revision": "0b9c51f718a0f13a3f5c",
+    "url": "/quiz/static/js/2.ca6faa92.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/quiz/static/js/2.ca6faa92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c1242facc853fc0988",
+    "url": "/quiz/static/js/main.38f1832a.chunk.js"
+  },
+  {
+    "revision": "dd7b1f3c1be70c4005d9",
+    "url": "/quiz/static/js/runtime-main.092bbaa6.js"
+  },
+  {
+    "revision": "59730d760c58195495f8e949911f213c",
+    "url": "/quiz/static/media/polytech.59730d76.jpg"
+  }
+]);
